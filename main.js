e1d2ece974d22c76176b6a80b5c25266ba680027
Ong'owo <76827981+mwafreaker@users.noreply.github.com>
2022-12-25 04:33:22 +0300

    plain commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const btn = document.querySelector(".header-btn")
+const hero = document.querySelector(".hero")
+
+const handleClick = () => {
+    btn.addEventListener("click", console.log("hello classList"))
+}
